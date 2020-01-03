bdb9363821f302064eb85290aa2e51ad94742b71
irfanmasoudi <irfanmasoudi@gmail.com>
2020-01-03 19:09:27 +0700

    New deleted method

--- lib/controller/reads_controller.dart
+++ lib/controller/reads_controller.dart
@@ -57,7 +57,7 @@ class ReadsController extends ResourceController {
     final int deleteCount = await readQuery.delete();
 
     if (deleteCount == 0) {
-      return Response.notFound(body: 'not found hmmmmm.');
+      return Response.notFound(body: 'Item has deleted and not found right now.');
     }
     return Response.ok('Deleted $deleteCount items.');
   }
